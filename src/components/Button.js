e5d380671db810e71251117a909c78f08995b94c
Travis Tingle <travt7@gmail.co>
2023-08-07 12:45:53 -0400

    create Button component
    create a button component that will be used in multiple places in our application.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import className from 'classnames';
 import { GoSync } from 'react-icons/go';
 
+//create a button component that will be used in multiple places in our application.
 function Button({
   children,
   primary,
